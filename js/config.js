524288aab4f948ff89a6cd163bed8552fb0783b2
ScottyErwin <mrscotty.erwin@gmail.com>
2026-05-22 16:01:01 -0700

    Simplified code to only load background.

--- js/config.js
+++ js/config.js
@@ -2,13 +2,6 @@ const config = {
     type: Phaser.AUTO,
     width: 1280,
     height: 720,
-    backgroundColor: '#5c9e31',
-    physics: {
-        default: 'arcade',
-        arcade: {
-            gravity: { y: 0 },
-            debug: false
-        }
-    },
-    scene: [BootScene, FarmScene]
+    backgroundColor: '#000000',
+    scene: [FarmScene]
 };
--- js/scenes/FarmScene.js
+++ js/scenes/FarmScene.js
@@ -4,26 +4,31 @@ class FarmScene extends Phaser.Scene {
     }
 
     create() {
-        // Simple green farm background
+        // ============================================
+        // FARM BACKGROUND - Starting Area Scenery
+        // ============================================
+
+        // Main grass background
         this.add.rectangle(640, 360, 1280, 720, 0x5c9e31);
 
-        // Title
-        this.add.text(640, 50, 'Monster Farm Smash', {
-            fontSize: '32px',
-            fill: '#fff'
+        // Simple dirt path (horizontal)
+        this.add.rectangle(640, 400, 900, 60, 0x8B5A2B);
+
+        // Simple dirt path (vertical)
+        this.add.rectangle(400, 360, 60, 500, 0x8B5A2B);
+
+        // Title text (temporary)
+        this.add.text(640, 60, 'Monster Farm Smash', {
+            fontSize: '36px',
+            fill: '#ffffff',
+            fontStyle: 'bold'
         }).setOrigin(0.5);
 
-        // Player (Kid)
-        this.player = new Player(this, 400, 300);
-        
-        // Twiggy
-        this.twiggy = new Monster(this, 450, 320, 'Twiggy');
+        // TODO: Add trees here later
+        // TODO: Add player here later
+        // TODO: Add Twiggy (monster) here later
+        // TODO: Add rocks, house, campfire, etc. later
 
-        console.log("FarmScene loaded - Use Arrow Keys or WASD to move");
-    }
-
-    update() {
-        this.player.update();
-        this.twiggy.update();
+        console.log('%c[FarmScene] Background loaded successfully', 'color: #0f0');
     }
 }
